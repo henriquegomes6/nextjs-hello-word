aea2778047c32e91eb1cccba98c83c0500d56128
henriquegomes6 <henriquegomes6@hotmail.com>
2020-06-24 00:43:55 -0300

    teste 1

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -4,7 +4,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
   res.statusCode = 200
-  res.json({ name: 'John Doe' })
+  res.json({ name: 'Nome aqui' })
 }
 
 export default handler
